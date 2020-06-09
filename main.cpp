bee91c7e33802dc4b140a92777dac1f5446d6bce
Daniil Kovalev <kovdan01@yandex.ru>
2020-06-09 10:57:48 +0300

    Add print_ip for container, string and tuple types

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "print_ip.h"
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <tuple>
 
 int main()
 {
@@ -10,5 +15,13 @@ int main()
     my::print_ip(int(2130706433));
     std::cout << std::endl;
     my::print_ip(long(8875824491850138409));
+    std::cout << std::endl;
+    my::print_ip(std::string{"trololo"});
+    std::cout << std::endl;
+    my::print_ip(std::vector{"print", "from", "vector"});
+    std::cout << std::endl;
+    my::print_ip(std::list{"print", "from", "list"});
+    std::cout << std::endl;
+    my::print_ip(std::make_tuple("print", "from", "tuple"));
     std::cout << std::endl;
 }
--- print_ip.h
+++ print_ip.h
@@ -1,9 +1,14 @@
 #ifndef PRINT_IP_H
 #define PRINT_IP_H
 
+#include "meta_functions.h"
+
 #include <type_traits>
 #include <iostream>
 #include <cstdint>
+#include <iterator>
+#include <algorithm>
+#include <tuple>
 
 namespace my
 {
@@ -31,6 +36,59 @@ void print_ip(Integer ip, std::ostream& stream = std::cout)
         stream << ((mask & u_ip) >> cur_shift);
         if (cur_shift != 0)
             stream << '.';
+    }
+}
+
+template <typename Container,
+          typename Fake = std::enable_if_t<meta::has_iterator_v<Container> && !meta::is_string_v<Container>, void>>
+void print_ip(const Container& ip, std::ostream& stream = std::cout)
+{
+    if (ip.empty())
+        return;
+    const char* delim = ".";
+    std::copy(ip.begin(), std::prev(ip.end()),
+              std::ostream_iterator<typename Container::value_type>(stream, delim));
+    stream << *ip.rbegin();
+}
+
+template <typename String,
+          typename Fake = std::enable_if_t<meta::is_string_v<String>, void>>
+void print_ip(const String& ip, std::ostream& stream = std::cout, Fake* /*placeholder*/ = nullptr)
+{
+    stream << ip;
+}
+
+namespace detail
+{
+
+template <std::size_t index, typename... Args>
+void print_ip_tuple_helper(const std::tuple<Args...>& ip, std::ostream& stream = std::cout)
+{
+    if constexpr (index >= sizeof...(Args))
+    {
+        return;
+    }
+    else
+    {
+        stream << '.' << std::get<index>(ip);
+        print_ip_tuple_helper<index + 1, Args...>(ip, stream);
+    }
+}
+
+} // namespace detail
+
+template <typename... Args,
+          typename Fake = std::enable_if_t<meta::check_tuple_v<Args...>, void>>
+void print_ip(const std::tuple<Args...>& ip, std::ostream& stream = std::cout)
+{
+    if constexpr (std::tuple_size<std::tuple<Args...>>::value == 0)
+    {
+        return;
+    }
+    else
+    {
+        stream << std::get<0>(ip);
+        detail::print_ip_tuple_helper<1, Args...>(ip, stream);
     }
 }
 
